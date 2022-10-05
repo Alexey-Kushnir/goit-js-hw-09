60e7e5d7601844447cb7897eb5e6ea9062e8a5f3
Alexey-Kushnir <al7787@gmail.com>
2022-10-05 18:55:43 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.body;
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerId = null;
 
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+
+  timerId = setInterval(() => {
+    const randomCollor = getRandomHexColor();
+    body.style.backgroundColor = randomCollor;
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
