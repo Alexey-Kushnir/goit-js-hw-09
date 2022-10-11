44a5669eb3b4dc01b0dc9c9b410d745c3dfd5a9a
Alexey-Kushnir <al7787@gmail.com>
2022-10-12 00:34:06 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,53 @@
 import Notiflix from 'notiflix';
-// Notiflix.Notify.warning('Please choose a date in the future');
+
+const form = document.querySelector('.form');
+const inputDelay = document.querySelector('input[name="delay"]');
+const inputStep = document.querySelector('input[name="step"]');
+const inputAmount = document.querySelector('input[name="amount"]');
+// const btnCreate = document.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  let position = 0;
+  let delayStep = 0;
+  for (let i = 0; i < Number(inputAmount.value); i += 1) {
+    position += 1;
+    delayStep = Number(inputDelay.value) + Number(inputStep.value);
+
+    createPromise(position, delayStep)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        // Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        // Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    console.log('start');
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
