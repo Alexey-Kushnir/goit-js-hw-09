1bb9591e90af5124674b22736f845aa4083c93bd
Alexey-Kushnir <al7787@gmail.com>
2022-10-13 00:14:27 +0300

    corrects

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,16 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
+
 form.addEventListener('submit', submitForm);
 
 function submitForm(e) {
   e.preventDefault();
+
   let {
     elements: { delay, step, amount },
   } = e.currentTarget;
+
   const inputDelay = Number(delay.value);
   const inputStep = Number(step.value);
   const inputAmount = Number(amount.value);
@@ -15,17 +18,13 @@ function submitForm(e) {
 
   for (let i = 1; i <= inputAmount; i += 1) {
     let promisePosition = i;
-    // console.log(delayStep);
+
     createPromise(promisePosition, delayStep)
       .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
-        );
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
-        );
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
       });
     delayStep += inputStep;
   }
